f32d54cbbe0c7dd019fd21b53f2c025bb6f173bf
corkesw <stephen.corke@northcoders.com>
2022-04-12 15:32:22 +0100

    add refactor to forEach

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ exports.captcha = (num) => {
   if (num < 10) return 0;
   let total = 0;
   const numberArray = num.split("");
-
   // to deal with checking that the first and latch match, add the first number onto the end of the array
   numberArray.push(numberArray[0]);
-  for (let i = 0; i < numberArray.length; i++) {
-    // at each index, if there is a match with the next index, add that number to the total
-    if (numberArray[i] === numberArray[i + 1]) {
-      total += +numberArray[i];
-    }
-  }
+  // compare each number with the next and add to total if they are the same
+  numberArray.forEach((number, index) => {
+    if (number === numberArray[index + 1]) total += +number;
+  });
+
   return total;
 };
 
