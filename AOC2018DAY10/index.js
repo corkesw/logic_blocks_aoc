2ae784e3724433235bd23f86aed4978ab29a6242
corkesw <stephen.corke@northcoders.com>
2022-05-23 09:10:53 +0100

    add comments

--- AOC2018DAY10/index.js
+++ AOC2018DAY10/index.js
@@ -7,11 +7,19 @@ const {
 } = require("./utils");
 
 const getPattern = (array, seconds) => {
+    // change x and y positions by x and y speed multiplied by a number of seconds
   const movedStars = addMovements(array, seconds);
+
+  // renumber the coordinates by the same x and y differentials so they are all positive
   const zeroedObjects = zeroObjects(movedStars);
+
+  // create an array of length y of strings of length x
   const stringsArray = createStrings(zeroedObjects);
+
+  // iterate through coordinates and replace the appropriate '.' with '#' to create plot
   const starPlot = plotStars(stringsArray, zeroedObjects);
-  console.dir(starPlot, { maxArrayLength: null });
+
+  return starPlot
 };
 
-getPattern(data, 10511);
+console.dir(getPattern(data, 10511), { maxArrayLength: null });
